d1eeb6fbee069f13d699e11740a350fe638c2a31
eddyyucca <eddyyucca@gmail.com>
2021-08-09 15:28:40 +0800

    mudahan final
    Signed-off-by: eddyyucca <eddyyucca@gmail.com>

--- application/views/atk/excel.php
+++ application/views/atk/excel.php
@@ -3,24 +3,41 @@ header("Content-type: application/vnd-ms-excel");
 $date = date('Y-m-d');
 header("Content-Disposition: attachment; filename=Data Barang $date.xls");
 ?>
-<table border="1">
+
+<table border="0">
     <tr align="center" width="400">
-        <td colspan="4"><img src="<?= base_url('assets/cop.png') ?>" width="90%"></td>
+        <td colspan="4"><img src="<?= base_url('assets/cop.png') ?>" width="50%"></td>
     </tr>
     <tr>
-        <td><br><br><br><br><br><br><br></td>
+        <td><br><br>
+        <td><br><br>
     </tr>
+    <tr>
+        <td><br><br>
+        <td><br><br>
+    </tr>
+
+</table>
+<table border="1">
     <tr align="center">
-        <td colspan="4">
-            <h2>DATA ATK</h2>
+        <td colspan="9">
+            DATA ATK
         </td>
     </tr>
 
     <tr>
-        <td>No.</td>
-        <td>Item</td>
-        <td>Jumlah</td>
-        <td>Satuan</td>
+        <td width="90%">
+            No.
+        </td>
+        <td width="90%" colspan="5">
+            Item
+        </td>
+        <td width="90%" colspan="2">
+            Jumlah
+        </td>
+        <td width="90%" colspan="1">
+            Satuan
+        </td>
     </tr>
 
     <?php $no = 1; ?>
@@ -28,10 +45,18 @@ header("Content-Disposition: attachment; filename=Data Barang $date.xls");
         <?php foreach ($data as $x) { ?>
 
 
-            <td align="center"><?= $no++; ?></td>
-            <td><?= $x->item; ?></td>
-            <td align="center"><?= $x->qty; ?></td>
-            <td><?= $x->satuan; ?></td>
+            <td align="center" width="90%">
+                <?= $no++; ?>
+            </td>
+            <td width="90%" colspan="5">
+                <?= $x->item; ?>
+            </td>
+            <td colspan="2" align="center" width="90%">
+                <?= $x->qty; ?>
+            </td>
+            <td colspan="1">
+                <?= $x->satuan; ?>
+            </td>
     </tr>
 <?php } ?>
 </table>
